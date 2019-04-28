3f9033716e4dbeab23c6f4758a14cb925a87e227
christian212519 <penrod.christian7984@gmail.com>
2019-04-28 18:46:54 -0400

    Created splashscreen

---
+++ app/src/main/java/com/app/linio/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.app.linio;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        launchSplashScreen();
+    }
+
+    private void launchSplashScreen() {
+        Thread timer = new Thread() {
+            @Override
+            public  void run() {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                } catch (Exception e) {
+                    Toast.makeText(SplashScreen.this,"Unable to load splash screen",Toast.LENGTH_LONG).show();
+                }
+                Intent main = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(main);
+            }
+        };
+        timer.start();
+    }
+}
